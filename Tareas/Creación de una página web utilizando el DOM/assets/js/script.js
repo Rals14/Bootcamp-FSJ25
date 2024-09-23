06350acf66f70bc989a26857f5807f0906ccb7cf
Rals <raulgallardo6@gmail.com>
2024-09-22 21:03:39 -0600

    adding dom events to dom activity

--- Tareas/Creación de una página web utilizando el DOM/assets/js/script.js
+++ Tareas/Creación de una página web utilizando el DOM/assets/js/script.js
@@ -1,3 +1,4 @@
+/*Función para cambiar color de fondo*/
 const mainBoton = document.querySelector('#main-btn');
 mainBoton.addEventListener('click', () => {
     const mainContent = document.querySelector('#main-content');
@@ -11,3 +12,45 @@ mainBoton.addEventListener('click', () => {
 
 });
 
+/*Funcion para mostrar más de la review*/
+const reviewBoton = document.querySelector('#review-btn');
+reviewBoton.addEventListener('click', () => {
+    const extra = document.querySelector('.game-review-extra');
+    if (extra.classList.contains('hidden')) {
+        extra.classList.remove('hidden')
+        reviewBoton.classList.add('hidden')
+    }    
+});
+
+/*Funcion para mostrar menos de la review*/
+const reviewBotonExtra = document.querySelector('#review-btn-extra');
+reviewBotonExtra.addEventListener('click', () => {
+    const extra = document.querySelector('.game-review-extra');
+    if (!extra.classList.contains('hidden')) {
+        extra.classList.add('hidden')
+        reviewBoton.classList.remove('hidden')
+    }    
+});
+
+/*Funcion para cambiar imagen de review*/
+const reviewImg = document.querySelector('#game-review-img');
+let i = 1;
+reviewImg.addEventListener('click', () => {
+    i++;
+    if (i > 4) {
+        i = 1;
+    }
+    reviewImg.src = `./assets/img/zelda${i}.jpg`;
+});
+    
+
+const form = document.querySelector('#subscribe-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.querySelector('#email');
+    if (email.value === '') {
+        alert('Por favor, ingrese un correo electrónico');
+    } else {
+        alert('Gracias por suscribirte a nuestro blog :D');
+    }
+});
